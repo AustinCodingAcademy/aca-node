249b9681be59b257789cebbdda69418bddb5432d
Alan Houston <alanhouston5000@gmail.com>
2019-01-16 12:36:22 -0600

    initial aca-node

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+"use strict";
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout  
+})
+
+rl.question('',(answer)=>{
+    let splitAnswer = answer.split('');
+    console.log(splitAnswer);
+    rl.close();
+})
