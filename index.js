1c51b80eac5e4f7ae26ee669ad05c9b384661e7a
Alan Houston <alanhouston5000@gmail.com>
2019-01-16 20:10:57 -0600

    all symbols working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//remove any spaces?
+//separate numerical inputs into their own variable?
+//.find or .filter each math/bool symbol
+//.join once solved
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -9,11 +14,53 @@ const rl = readline.createInterface({
 
 const openPrompt=()=>{
     rl.question('',(answer)=>{
-        let splitAnswer = answer.split('');
-        console.log(splitAnswer);
+        let splitAnswer = answer.split(' ');
+        //trim spaces here
+        // console.log(splitAnswer)
+        lookForSymbol(splitAnswer);
+        // splitUpInputs(splitAnswer);
+        // console.log(splitAnswer);
         openPrompt();
-        rl.close();
+        // rl.close();
     });
+}
+
+const lookForSymbol=(splitArray)=>{
+    const theSymbol = splitArray[1]
+    const firstInput = Number(splitArray[0]);
+    const secondInput = Number(splitArray[2]);
+    if(theSymbol === '+'){
+        console.log(firstInput + secondInput)
+    }
+    if(theSymbol === '-'){
+        console.log(firstInput - secondInput)
+    }
+    if(theSymbol === '*'){
+        console.log(firstInput * secondInput)
+    }
+    if(theSymbol === '/'){
+        console.log(firstInput / secondInput)
+    }
+    if(theSymbol === '>'){
+        if(firstInput > secondInput){
+            console.log('true')
+        }else console.log('false')
+    }
+    if(theSymbol === '<'){
+        if(firstInput < secondInput){
+            console.log('true')
+        }else console.log('false')
+    }
+    if(theSymbol === '==='){
+        if(firstInput === secondInput){
+            console.log('true')
+        }else console.log('false')
+    }
+    if(theSymbol === '!=='){
+        if(firstInput !== secondInput){
+            console.log('true')
+        }else console.log('false')
+    }
 }
 
 const startNode=()=>{
