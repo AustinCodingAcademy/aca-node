7db706b17c67fc3d2d401a6e08c30357245a55ec
Darwin Risser <riser.darwin@gmail.com>
2019-01-20 15:41:09 -0600

    mostly working v

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function doMath () {
+   rl.question('', (answer) => {
+    var newArr = answer.split(' ');
+    var num1 = Number(newArr[0]);
+    var operator = newArr[1];
+    var num2 = Number(newArr[2]);
+    if (operator === '+'){
+      console.log(num1 + num2);
+    } else if (operator === '-'){
+      console.log(num1 - num2);
+    } else if (operator === '/'){
+      console.log(num1 / num2)
+    } else if (operator === '*'){
+      console.log(num1 * num2)
+    } else if (operator === '>'){
+      if (num1 > num2){
+        console.log(true)
+      } else {
+        console.log(false)
+      }
+    } else if (operator === '<'){
+      if (num1 < num2){
+        console.log(true)
+      } else {
+        console.log(false)
+      }
+    } else if (operator === '==='){
+      if (num1 === num2){
+        console.log(true)
+      } else {
+        console.log(false)
+      }
+    } 
+  });
+}
+
+doMath();
