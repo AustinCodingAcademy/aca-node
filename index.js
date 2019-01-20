abe200e909c57bc3affc4f56027d112475b46d1a
Darwin <risser.darwin@gmail.com>
2019-01-20 11:29:16 -0600

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const readline = require('readline-sync');
 
+var firstNum = readline.question("Please enter a number ");
+var secondNum = readline.question("Please enter a second number ");
+var operator = readline.question("Please enter +, -, *, or / ")
+
+function calculate(){
+   if(operator === '+'){
+       console.log(parseInt(firstNum) + parseInt(secondNum))
+   }
+
+}
+
+calculate();
