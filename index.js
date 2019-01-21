07836260179081426405bfe6b3855c0235954e9b
Alan Houston <alanhouston5000@gmail.com>
2019-01-21 17:24:29 -0600

    spacing no longer an issue, but variable not coded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //remove any spaces?
 //separate numerical inputs into their own variable?
 //.find or .filter each math/bool symbol
-//.join once solved
+// .join once solved?
 
 const readline = require('readline');
 
@@ -12,10 +12,12 @@ const rl = readline.createInterface({
     output: process.stdout  
 })
 
-const openPrompt=()=>{
+function openPrompt(){
     rl.question('',(answer)=>{
-        let splitAnswer = answer.split(' ');
-        separateStuff(splitAnswer);
+        let splitAnswer = answer.split('');
+        // console.log(splitAnswer)
+        // separateStuff(splitAnswer);
+        separateStuff(answer);
         openPrompt();
     });
 }
@@ -25,54 +27,79 @@ const variable = {
     value: '',
 }
 
-const separateStuff=(splitArray)=>{
-    const indexZero = splitArray[0];
-    const indexOne = splitArray[1]
-    const indexTwo = splitArray[2];
-    const indexThree = splitArray[3];
-    if(!isNaN(Number(indexZero)) && !isNaN(Number(indexTwo))){
-        if(indexOne === '+'){
-            console.log(Number(indexZero) + Number(indexTwo))
-        }
-        if(indexOne === '-'){
-            console.log(Number(indexZero) - Number(indexTwo))
-        }
-        if(indexOne === '*'){
-            console.log(Number(indexZero) * Number(indexTwo))
-        }
-        if(indexOne === '/'){
-            console.log(Number(indexZero) / Number(indexTwo))
-        }
-        if(indexOne === '>'){
-            if(Number(indexZero) > Number(indexTwo)){
-                console.log('true')
-            }else console.log('false')
-        }
-        if(indexOne === '<'){
-            if(Number(indexZero) < Number(indexTwo)){
-                console.log('true')
-            }else console.log('false')
-        }
-        if(indexOne === '==='){
-            if(Number(indexZero) === Number(indexTwo)){
-                console.log('true')
-            }else console.log('false')
-        }
-        if(indexOne === '!=='){
-            if(Number(indexZero) !== Number(indexTwo)){
-                console.log('true')
-            }else console.log('false')
-        }
-    }else if(indexZero === 'let' && indexTwo === '='){
-        variable.value = indexThree;
-        variable.name = indexOne;
-    }else if(indexZero === variable.name){
-        console.log(variable.value)
-    }
-    // else if(){
+let partsOfInput = {
+    leftSide: [],
+    operator: [],
+    rightSide: []
+}
 
-    // }
+function clearPartsOfInput(){
+    partsOfInput.leftSide = [];
+    partsOfInput.rightSide = [];
+    partsOfInput.operator = [];
+}
+
+function separateStuff(answer){
+    // const indexZero = splitArray[0];
+    // const indexOne = splitArray[1]
+    // const indexTwo = splitArray[2];
+    // const indexThree = splitArray[3];
+    let noSpaceArray = answer.trim().replace(/\s+/g,'').split('');
+    let switchSides = false;
+    clearPartsOfInput();
+    noSpaceArray.forEach(item => {
+        if (!isNaN(Number(item)) && switchSides === false){
+            partsOfInput.leftSide.push(item);
+        }else if (!isNaN(Number(item)) && switchSides === true){
+            partsOfInput.rightSide.push(item);
+        }else if (item === '+' || '-' || '*' || '/' || '!' || '=' || '>' || '<'){
+            partsOfInput.operator.push(item);
+            switchSides = true;
+        }
+    });
+    let solidLeftSide = partsOfInput.leftSide.join('')
+    let solidRightSide = partsOfInput.rightSide.join('')
+    let solidOperator = partsOfInput.operator.join('')
+    //add if's for each index to be variable
+    if(solidOperator === '+'){
+        console.log(Number(solidLeftSide) + Number(solidRightSide))
+    }
+    else if(solidOperator === '-'){
+        console.log(Number(solidLeftSide) - Number(solidRightSide))
+    }
+    else if(solidOperator === '*'){
+        console.log(Number(solidLeftSide) * Number(solidRightSide))
+    }
+    else if(solidOperator === '/'){
+        console.log(Number(solidLeftSide) / Number(solidRightSide))
+    }
+    else if(solidOperator === '>'){
+        if(Number(solidLeftSide) > Number(solidRightSide)){
+            console.log('true')
+        }else console.log('false')
+    }
+    else if(solidOperator === '<'){
+        if(Number(solidLeftSide) < Number(solidRightSide)){
+            console.log('true')
+        }else console.log('false')
+    }
+    else if(solidOperator === '==='){
+        if(Number(solidLeftSide) === Number(solidRightSide)){
+            console.log('true')
+        }else console.log('false')
+    }
+    else if(solidOperator === '!=='){
+        if(Number(solidLeftSide) !== Number(solidRightSide)){
+            console.log('true')
+        }else console.log('false')
+    }
     else console.log('Syntax error')
+    // else if(indexZero === 'let' && indexTwo === '='){
+    //     variable.value = indexThree;
+    //     variable.name = indexOne;
+    // }else if(indexZero === variable.name){
+    //     console.log(variable.value)
+    // }
 }
 
 const startNode=()=>{
